4f39dbced17ae9653a9fac8a0d17d11c16852e42
David Kayo <david@kayoventures.com>
2017-12-21 11:51:15 -0800

    updated teacher.cs file

--- backend/models/Teacher.cs
+++ backend/models/Teacher.cs
@@ -10,11 +10,12 @@ namespace backend.Controllers
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string Bio { get; set; }
     public string Email { get; set; }
     public string GradeLevel { get; set; }
-    public string Bio { get; set; }
-    
-    // public int SchoolID {get; set;}
+
+
+    // public int SchoolId {get; set;}
 
   }
 
