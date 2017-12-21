2b78049f90660b3f679cb02da6e80bfca28f1eb6
Home <home@Homes-MBP.attlocal.net>
2017-12-21 13:46:02 -0800

    Added product class

---
+++ backend/models/Product.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace backend.Controllers{
+    
+    public class Product
+    {
+        public string Id { get; set; }
+        public string BrandName { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public bool InStock { get; set; }
+        public string Description { get; set; }
+        public int PatronId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
