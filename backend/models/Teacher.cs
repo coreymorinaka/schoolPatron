d93062302c5c5094288615e178004720605fbcd5
David Kayo <david@kayoventures.com>
2017-12-21 11:49:41 -0800

    Added Teacher.cs model

---
+++ backend/models/Teacher.cs
@@ -0,0 +1,21 @@
+using System;
+
+
+namespace backend.Controllers
+{
+
+  public class Teacher
+  {
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string GradeLevel { get; set; }
+    public string Bio { get; set; }
+    
+    // public int SchoolID {get; set;}
+
+  }
+
+}
