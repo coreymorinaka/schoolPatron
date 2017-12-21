0f880240fd0c99cd562a2406a98501a627e1a236
Home <home@Homes-MBP.attlocal.net>
2017-12-21 11:53:12 -0800

    Added project class

---
+++ backend/models/Teacher.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+namespace backend.Controllers
+{
+
+  public class Teacher
+  {
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Bio { get; set; }
+    public string Email { get; set; }
+    public string GradeLevel { get; set; }
+
+
+    // public int SchoolId {get; set;}
+
+  }
+
+}
