93da64024bfdbad10165fbe663d1dfaa05590934
Home <home@Homes-MBP.attlocal.net>
2017-12-21 15:23:32 -0800

    Formatted SchoolPatronContext

--- backend/SchoolPatronContext.cs
+++ backend/SchoolPatronContext.cs
@@ -1,28 +1,21 @@
 using System;
-using Microsoft.EntityFrameworkCore;
 using backend.Controllers;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
-  public class SchoolPatronContext : DbContext
-  {
-    public SchoolPatronContext(DbContextOptions<SchoolPatronContext> options) : base(options)
+    public class SchoolPatronContext : DbContext
     {
-<<<<<<< HEAD
-
-=======
-        public SchoolPatronContext(DbContextOptions<SchoolPatronContext> options) : base(options)
+        public SchoolPatronContext (DbContextOptions<SchoolPatronContext> options) : base (options)
         {
-            
+
         }
         public DbSet<Patron> patrons { get; set; }
+        public DbSet<School> schools { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
         public DbSet<Product> products { get; set; }
         public DbSet<Project> projects { get; set; }
->>>>>>> 8edcb112b821556ff62343cd011cbf6c61bccfc5
-    }
-    public DbSet<Patron> patrons { get; set; }
-    public DbSet<School> schools { get; set; }
-    public DbSet<Teacher> teachers { get; set; }
 
-  }
+    }
+
 }
