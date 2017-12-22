a0d54fcef7326619b4569f3e9051c44b7e88a295
Home <home@Homes-MBP.attlocal.net>
2017-12-22 15:44:16 -0800

    Added school id to teacher class

--- backend/models/Teacher.cs
+++ backend/models/Teacher.cs
@@ -1,12 +1,9 @@
 using System;
 
-
 namespace backend.Controllers
 {
-
   public class Teacher
   {
-
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
@@ -14,10 +11,6 @@ namespace backend.Controllers
     public string Email { get; set; }
     public string GradeLevel { get; set; }
     public string Password { get; set; }
-
-
-    // public int SchoolId {get; set;}
-
+    public int SchoolId { get; set; }
   }
-
 }
