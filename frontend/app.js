a267f36c94b6f6e8a2378ec8fb407256c275af82
David Kayo <david@kayoventures.com>
2017-12-24 09:19:27 -0800

    Updating template url for project

--- frontend/app.js
+++ frontend/app.js
@@ -68,7 +68,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state("projects", {
       url: "/projects",
-      templateUrl: "./views/projectsView.html",
+      templateUrl: "./views/projectView.html",
       controller: "projectController"
     })
   $stateProvider
@@ -80,7 +80,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state("project", {
       url: "/project/:id",
-      templateUrl: "./views/projectProfileView.html",
+      templateUrl: "./views/projectView.html",
       controller: "projectController"
     })
   $stateProvider
