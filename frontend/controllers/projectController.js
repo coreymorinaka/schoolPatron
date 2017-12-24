1dfb7dfe6c16982ca3bd79d1b627c2e19e318c5d
Home <home@Homes-MacBook-Pro.local>
2017-12-24 09:41:20 -0800

    Started CRUD function on front end for projects

---
+++ frontend/controllers/projectController.js
@@ -0,0 +1,70 @@
+app.controller("projectController", function ($scope, $state, $stateParams, $http, projectService) {
+
+    if ($stateParams.id == null || $stateParams.id == "" || $stateParams.id == undefined) {
+        $scope.project = {
+            id: 0,
+            Name: "",
+            StartDate: "",
+            EndDate: "",
+            Subject: "",
+            GradeLevel: "",
+            Description: "",
+            GoalReached: false,
+            TeacherId: null,
+            SchoolId: null
+        }
+
+        $scope.heading = "Create a Project";
+        $scope.submitButton = true;
+    }
+    else {
+        projectService.getProjectById($stateParams.id)
+            .then(function (response) {
+                console.log(response.data);
+                $scope.project = response.data;
+                $scope.heading = "Update your Project!";
+                $scope.submitButton = false;
+                console.log($scope.project);
+            }, function (error) {
+                console.log(error);
+            })
+    }
+        projectService.getProjects()
+            .then(function (response) {
+                console.log(response);
+                $scope.projects = response.data
+            }, function (error) {
+                console.log(error);
+            })
+
+    $scope.addProject = function () {
+        projectService.addProject($scope.project)
+            .then(function (response) {
+                console.log(response);
+                $state.go("project", {id: response.data.id});
+            }, function (error) {
+                console.log(error);
+            })
+    }
+
+    $scope.updateProject = function () {
+        console.log($stateParams.id);
+        console.log($scope.project);
+        projectService.updateProject($scope.project.id, $scope.project)
+            .then(function (response) {
+                console.log(response);
+                $state.go("project", {id: $stateParams.id})
+            }, function (error) {
+                console.log(error);
+            })
+    }
+    $scope.deleteProject = function () {
+        projectService.deleteProject($scope.project.id)
+            .then(function (response) {
+                console.log(response);
+                $state.go("projects");
+            }, function (error) {
+                console.log(error);
+            })
+    }
+})
